f7a6b920031f63408e8c5b05389b0c854cc9cd6a
Sarthak <sarthakbhatt@live.com>
2024-03-31 04:18:45 +0530

    table created using pg

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,17 @@ const client = new Client({
   connectionString: `postgresql://culsarthak:DmMQ0GReiY4T@ep-yellow-hall-a59f5gsn.us-east-2.aws.neon.tech/week10_study_postgres?sslmode=require`,
 });
 
-client.connect();
-
 const createUsersTable = async () => {
+  await client.connect();
   const result = await client.query(`
-  CREATE TBALE users(
+  CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP WITH TIMEZONE DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
   )`);
   console.log(result);
 };
+
+createUsersTable();
